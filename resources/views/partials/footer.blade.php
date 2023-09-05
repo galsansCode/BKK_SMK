47bdc7be667c3bd93da98cfc035a7c2883447761
galsans <glhsmk21357@gmail.com>
2023-09-05 09:30:01 +0700

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,53 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+      <div class="container">
+          <div class="row gy-4">
+              <div class="col-lg-5 col-md-12 footer-info">
+                  <a href="index.html" class="logo d-flex align-items-center">
+                      <span>SI BKK</span>
+                  </a>
+                  <p>Sistem informasi BKK (Bursa Kerja Khusus) merupakan sebuah tempat untuk alumni.non-alumni untuk
+                      mendapatkan pekerjaan</p>
+                  <div class="social-links d-flex mt-4">
+                      <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                      <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                      <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                      <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                  </div>
+              </div>
+
+              <div class="col-lg-3 col-6 footer-links">
+                  <h4>Menu</h4>
+                  <ul>
+                      <li><a href="/">Home</a></li>
+                      <li><a href="/lowongan">Lowongan</a></li>
+                      <li><a href="/pengumuman">Pengumuman</a></li>
+                      <li><a href="/dashboard">dashboard</a></li>
+                  </ul>
+              </div>
+
+
+              <div class="col-lg-4 col-md-12 footer-contact text-center text-md-start">
+                  <h4>Kontak Kami</h4>
+                  <p>
+                      Jl. Batu III, RT.6/RW.1, Gambir<br>
+                      Kecamatan Gambir, Kota Jakarta Pusat,<br>
+                      Daerah Khusus Ibukota <br><br>
+                      <strong>Phone:</strong> +62-8810-2434-5979<br>
+                      <strong>Email:</strong> galihputra4.221@gmail.com<br>
+                  </p>
+
+              </div>
+
+          </div>
+      </div>
+
+      <div class="container mt-4">
+          <div class="copyright">
+              &copy; Copyright <strong><span>Galih</span></strong>. All Rights Reserved
+          </div>
+      </div>
+
+  </footer><!-- End Footer -->
+  <!-- End Footer -->
